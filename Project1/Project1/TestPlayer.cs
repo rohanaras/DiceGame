8c38b71d14e76639ac68034fc930ae424c864773
Rohan Aras <tchompsig@gmail.com>
2014-12-20 20:45:30 -0800

    Created barebone testplayer

--- Project1/Project1/TestPlayer.cs
+++ Project1/Project1/TestPlayer.cs
@@ -11,16 +11,35 @@ namespace Project1
 {
     class TestPlayer : Player
     {
+        private List<int[]> calls;
 
         public TestPlayer(Hand hand) : base(hand) { }
 
         public override void GameState(List<int[]> calls, List<string> names, Dictionary<string, int> diceLeft)
         {
+            this.calls = calls;
         }
 
         public override int[] Call()
         {
-            throw new NotImplementedException();
+            if (calls.Count != 0)
+            {
+                int[] previous = new int[2];
+                previous[0] = calls[calls.Count - 1][0] + 1;
+                previous[1] = calls[calls.Count - 1][1] + 1;
+                if (previous[1] <= 6)
+                    return previous;
+                else
+                {
+                    previous[1] = 2;
+                    return previous;
+                }
+            }
+            else
+            {
+                int[] thisCall = {1, 2};
+                return thisCall;
+            } 
         }
     }
 }
